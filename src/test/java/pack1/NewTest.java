c6363b099716ea65f27b233f344bd9332e7005e5
Vilas Paliwal <vivek.vilas.paliwal@accenture.com>
2019-03-26 14:52:26 +0530

    adding maven project

--- src/test/java/pack1/NewTest.java
+++ src/test/java/pack1/NewTest.java
@@ -1,11 +1,26 @@
 package pack1;
-
+ 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
+ 
 public class NewTest {
   @Test
   public void f() {
-	  System.out.println("this is my test project");
-	  
+         System.out.println("This is my test project");
+         System.setProperty("webdriver.chrome.driver","C:\\Users\\vivek.vilas.paliwal\\Downloads\\chromedriver_win32");
+         //launch chrome browser
+         WebDriver driver=new ChromeDriver();
+         //open the application
+         driver.get("http://newtours.demoaut.com/");
+         
+         //enter username
+         driver.findElement(By.name("userName")).sendKeys("mercury");
+         driver.findElement(By.name("password")).sendKeys("mercury");
+         driver.findElement(By.name("login")).click();
+         driver.close();
+         
   }
-}
+} 
+ 
